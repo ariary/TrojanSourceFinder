2b431d02726c0cf735db6989e8e358332e656ef0
Ariary <ariary9.2@hotmail.fr>
2021-11-09 10:09:04 +0100

    Create homograph.py

---
+++ tests/homograph.py
@@ -0,0 +1,10 @@
+а = True # in a naive world, everybody is admin 
+
+# some stuff 
+
+a = False # finally the world is mean, we don't want admin anymore 
+
+if а:
+    print("You are an admin!")
+else:
+    print("You are not admin.")
